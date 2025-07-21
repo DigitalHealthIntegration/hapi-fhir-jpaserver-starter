b8588999a799f142789ed3477aba91deda19f172
AdityaKBhadragond14 <adityab@iprdgroup.com>
2025-07-21 17:56:21 +0530

    code refactor

--- src/main/java/ca/uhn/fhir/jpa/starter/service/TusService.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/service/TusService.java
@@ -36,7 +36,8 @@ public class TusService {
 			// Execute the transfer via the selected strategy
 			fileStrategyContext.executeStrategy(uploadUrl);
 		} catch (IllegalStateException e){
-			logger.info("Failed to execute strategy for upload: " + uploadUrl + e.getMessage());
+			logger.info("Failed to execute strategy for upload: " + uploadUrl);
+			logger.info(e.getMessage());
 		}
 	}
 
