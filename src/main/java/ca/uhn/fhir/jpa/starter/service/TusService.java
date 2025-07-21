13ecfbb58c6137101a548dca505c926334deb53a
AdityaKBhadragond14 <102521117+AdityaKBhadragond14@users.noreply.github.com>
2025-07-21 12:37:32 +0000

    Handle invalid strategy for TUS upload (#105)
    * Handle Invalid File Strategy for TUS upload
    
    * code refactor

--- src/main/java/ca/uhn/fhir/jpa/starter/service/TusService.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/service/TusService.java
@@ -30,10 +30,15 @@ public class TusService {
 	}
 
 	public void transferToFinalStorage(String uploadUrl, String fileType) throws Exception {
-		// Set strategy based on file type
-		fileStrategyContext.setFileStrategy(fileType);
-		// Execute the transfer via the selected strategy
-		fileStrategyContext.executeStrategy(uploadUrl);
+		try{
+			// Set strategy based on file type
+			fileStrategyContext.setFileStrategy(fileType);
+			// Execute the transfer via the selected strategy
+			fileStrategyContext.executeStrategy(uploadUrl);
+		} catch (IllegalStateException e){
+			logger.info("Failed to execute strategy for upload: " + uploadUrl);
+			logger.info(e.getMessage());
+		}
 	}
 
 	@Scheduled(initialDelay = INITIAl_DELAY, fixedDelay = FIXED_DELAY)
